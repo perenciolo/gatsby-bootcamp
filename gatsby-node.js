e4ec6d09bad260bf645224972b94221b0ddb80b2
Gustavo Henrique de Lima Perenciolo <gustavolima@lnxcit020062.cit>
2019-05-24 14:00:33 -0300

    Add Contentful CMS integration

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,44 +1,73 @@
 /**
  * Implements Gatsby's Node APIs.
  */
-const path = require('path')
+const path = require("path")
 
-module.exports.onCreateNode = ({ node, actions }) => {
-  const { createNodeField } = actions
-
-  if (node.internal.type === 'MarkdownRemark') {
-    const slug = path.basename(node.fileAbsolutePath, '.md')
-
-    createNodeField({
-      node,
-      name: 'slug',
-      value: slug
-    })
-  }
-}
-
+// CREATE CONTENTFUL PAGES
 module.exports.createPages = async ({ graphql, actions }) => {
   const { createPage } = actions
-  const blogTemplate = path.resolve('./src/templates/blog.js')
-  const res = await graphql(`query{
-    allMarkdownRemark {
-      edges {
-        node {
-          fields {
+  const blogTemplate = path.resolve("./src/templates/blog.js")
+  const res = await graphql(`
+    query {
+      allContentfulBlogPost {
+        edges {
+          node {
             slug
           }
         }
       }
     }
-  }`)
+  `)
 
-  res.data.allMarkdownRemark.edges.forEach(edge => {
+  res.data.allContentfulBlogPost.edges.forEach(edge => {
     createPage({
       component: blogTemplate,
-      path: `/blog/${edge.node.fields.slug}`,
+      path: `/blog/${edge.node.slug}`,
       context: {
-        slug: edge.node.fields.slug
-      }
+        slug: edge.node.slug,
+      },
     })
   })
 }
+
+// CREATE MARKDOWN SLUGS
+// module.exports.onCreateNode = ({ node, actions }) => {
+//   const { createNodeField } = actions
+
+//   if (node.internal.type === 'MarkdownRemark') {
+//     const slug = path.basename(node.fileAbsolutePath, '.md')
+
+//     createNodeField({
+//       node,
+//       name: 'slug',
+//       value: slug
+//     })
+//   }
+// }
+
+// CREATE MARKDOWN PAGES
+// module.exports.createPages = async ({ graphql, actions }) => {
+//   const { createPage } = actions
+//   const blogTemplate = path.resolve('./src/templates/blog.js')
+//   const res = await graphql(`query{
+//     allMarkdownRemark {
+//       edges {
+//         node {
+//           fields {
+//             slug
+//           }
+//         }
+//       }
+//     }
+//   }`)
+
+//   res.data.allMarkdownRemark.edges.forEach(edge => {
+//     createPage({
+//       component: blogTemplate,
+//       path: `/blog/${edge.node.fields.slug}`,
+//       context: {
+//         slug: edge.node.fields.slug
+//       }
+//     })
+//   })
+// }
